2bdec809d8a76896eca8049f3768fe2bbccd5420
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-10-14 15:37:06 +0100

    Manufacturing request:Prevent creating manufacuring ordre when the related prepress proof is «Quarantined»

--- models/mrp_production_request.py
+++ models/mrp_production_request.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError,ValidationError
 
 
 class MrpProductionRequest(models.Model):
@@ -33,11 +33,29 @@ class MrpProductionRequest(models.Model):
                 request.prepress_proof_id = prepress_proof and prepress_proof.ids[0] or False
         return production_order_requests
 
+    def action_make_production_order(self):
+        self._check_related_prepress_proof()
+        return super(MrpProductionRequest,self).action_make_production_order()
+
+
+    def _action_make_production_order(self, quantity=False, product_uom_id=False):
+        self._check_related_prepress_proof()
+        return super(MrpProductionRequest,self)._action_make_production_order(quantity=quantity,product_uom_id=product_uom_id)
+
+
+    def _check_related_prepress_proof(self):
+        for each in self:
+            if each.prepress_proof_id and  each.prepress_proof_id.state == 'quarantined':
+                raise ValidationError(_("Can not create manufacturing order for request %s ,the related prepress proof is quarantined!")%each.name)
+
     def _check_state(self, state):
         super(MrpProductionRequest,self)._check_state(state)
         for each in self:
             if state in ('validated', 'done') and each.prepress_proof_id and each.prepress_proof_id.state != 'validated':
                 raise UserError(_('The Prepress proof must be validated!'))
+
+
+
 
 
 
